12c1392fdf2fcd8d0f35bbf3b698713e427a432c
Guz117 <cristian.guzman117@gmail.com>
2021-11-23 15:19:56 +0100

    fizzbuzzdom

--- js/script.js
+++ js/script.js
@@ -8,9 +8,12 @@ for (let i = 1; i <= 100; i++) {
     container.append(div);
     if  (i % 3 == 0 && i % 5 == 0){
         div.classList.add('red');
+        div.innerHTML = 'fizzbuzz';
     } else if (i % 5 == 0) {
         div.classList.add('yellow');
+        div.innerHTML = 'buzz';
     } else if (i % 3 == 0) {
         div.classList.add('green');
+        div.innerHTML = 'fizz';
     }
 }
